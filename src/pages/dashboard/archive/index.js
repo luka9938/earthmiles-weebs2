72adaa17c96b499631cc7f2887db3dc95ea5aac7
Lukas G <luka9938@stud.kea.dk>
2023-11-15 14:59:55 +0100

    Active_indhold

---
+++ src/pages/dashboard/archive/index.js
@@ -0,0 +1,10 @@
+import styles from "./archive.module.css";
+
+export default function Archive() {
+  return (
+    <article className={styles.grid_container}>
+      <article className={styles.big_element}></article>
+      <article className={styles.big_element}></article>
+    </article>
+  );
+}
