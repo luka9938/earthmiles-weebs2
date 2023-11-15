c87c8663148790e8c8ece034638424fa448bfa9b
steffen kroyer <stef5450@stud.kea.dk>
2023-11-15 13:58:23 +0100

    dont know what im doing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import styles from "./Home.module.css";
 
 const App = () => {
   return (
-    <div className={styles.homeBody} style={{ backgroundImage: "url(#)" }}>
+    <div className={styles.homeBody} style={{ backgroundImage: "url(/foto2.jpeg)" }}>
       <div className={styles.homeText}>
         <h1>Earth Miles</h1>
         <h2>Velkommen til Earth Miles for partner</h2>
