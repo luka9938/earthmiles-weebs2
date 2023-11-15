624b97591eb059fcb4a120b16f89a5349a537bc8
steffen kroyer <stef5450@stud.kea.dk>
2023-11-15 13:42:49 +0100

    landing page igen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,19 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import React from "react";
 import styles from "./Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
-function Home() {
+const App = () => {
   return (
-    <div className={styles.landingpage}>
-      <img
-        src="image.jpg"
-        alt="Background Image"
-        className="Background-image"
-      ></img>
-      <div className={styles.content}>
-        <h1>Your heading</h1>
-        <p>Your descriptive text goes here.</p>
-        <div className="button">
-          <a href="#" className="button">
-            button 1
-          </a>
-          <a href="#" className="button">
-            button 2
-          </a>
+    <div className={styles.homeBody} style={{ backgroundImage: "url(#)" }}>
+      <div className={styles.homeText}>
+        <h1>Earth Miles</h1>
+        <h2>Velkommen til Earth Miles for partner</h2>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>Login</button>
+          <button className={styles.button}>Sign Up</button>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Home;
+export default App;
