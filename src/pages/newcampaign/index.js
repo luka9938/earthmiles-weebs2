a5f69b48f176284976a552f4fe2a2d3901fa8fc0
steffen kroyer <stef5450@stud.kea.dk>
2023-11-16 13:45:48 +0100

    stuff is going great :,)

---
+++ src/pages/newcampaign/index.js
@@ -0,0 +1,17 @@
+import "react";
+import styles from "./campaign.module.css";
+
+export default function Buyers({ data }) {
+  return (
+    <>
+      <title>Find buyer | EDC</title>
+
+      <div className={styles.wrapper}>
+        <h1 className={styles.h1}>I don't know what im doing</h1>
+        <div className={styles.content}>
+          <div className={styles.home}></div>
+        </div>
+      </div>
+    </>
+  );
+}
