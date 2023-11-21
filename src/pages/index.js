9560144f864569d5655faea22ecf9b65da15854b
steffen kroyer <stef5450@stud.kea.dk>
2023-11-21 12:44:45 +0100

    add grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import react from "react";
 import styles from "./Home.module.css";
 import Link from "next/link";
 
@@ -6,16 +6,25 @@ const App = () => {
   return (
     <div className={styles.homeBody} style={{ backgroundImage: "url(/foto2.jpeg)" }}>
       <div className={styles.homeText}>
-        <h1>Earth Miles</h1>
-        <h2>Velkommen til Earth Miles for partner</h2>
-        <div className={styles.buttonContainer}>
-          <Link href="./login" className={styles.button}>
-            Login
-          </Link>
-          <Link href="./createaccount" className={styles.button}>
-            Sign Up
-          </Link>
-        </div>
+        <h1>
+          Earth Miles
+          <br />
+          for partner
+        </h1>
+        <p>
+          In your orange shirt We'll go for yogurt Wish I could paint you, oh whoa-whoa I'd never do
+          you justice
+          <br /> Oh yeah! Oh yeah! I only wanted to be near you I only wanted to be close to someone
+          <br />I only wanted to be next to you I didn't think it through
+        </p>
+      </div>
+      <div className={styles.buttonContainer}>
+        <Link href="./login" className={styles.button}>
+          Login
+        </Link>
+        <Link href="./createaccount" className={styles.button}>
+          Sign Up
+        </Link>
       </div>
     </div>
   );
