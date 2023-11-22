f0f55e0d6af39936157d11dac8e63932807d8d65
steffen kroyer <stef5450@stud.kea.dk>
2023-11-22 13:53:15 +0100

    shit is begining to look godt ;)

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,9 @@ const App = () => {
       <div className={styles.imageContainer}>
         <img src={"/leftimage1.jpeg"} alt="Image1" className={styles.leftImage}></img>
       </div>
+
       <div className={styles.imageContainer}>
-        <img src={"/leftimage2.jpeg"} alt="Image2" className={styles.leftImage}></img>
+        <img src={"/leftimage2.jpeg"} alt="Image2" className={styles.leftImage2}></img>
       </div>
       <div className={styles.homeText}>
         <h1>
@@ -18,10 +19,13 @@ const App = () => {
           for partner
         </h1>
         <p>
-          In your orange shirt We'll go for yogurt Wish I could paint you, oh whoa-whoa I'd never do
-          you justice
-          <br /> Oh yeah! Oh yeah! I only wanted to be near you I only wanted to be close to someone
-          <br />I only wanted to be next to you I didn't think it through
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          <br />
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          <br /> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo,
+          <br />
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.
+          <br /> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
         </p>
       </div>
       <div className={styles.buttonContainer}>
