2cd8f9e245b47aa454eff507c5454599b3c1a452
Lukas G <luka9938@stud.kea.dk>
2023-11-28 13:36:12 +0100

    vercel fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import "../styles/fonts.css";
+import "../styles/fonts";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
