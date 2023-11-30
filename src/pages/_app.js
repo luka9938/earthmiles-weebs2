ea4dcd80f43e5ddad7616e272cecc44fc30be7f6
Lukas G <luka9938@stud.kea.dk>
2023-11-30 13:11:41 +0100

    new font bitches

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
-
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sofia+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Zilla+Slab:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
