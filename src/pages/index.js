86ff5d21507d70505d141940cf5b852f970ae923
Lukas G <luka9938@stud.kea.dk>
2023-11-30 13:44:03 +0100

    epic background

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,43 +15,55 @@ const App = () => {
   };
 
   return (
-    <div className={styles.homeBody} style={{ backgroundImage: "url(/placeholder1.jpg)" }}>
-      <div className={styles.imageContainer}>
-        <img
-          src={leftImageSource}
-          alt="Image1"
-          className={styles.leftImage}
-          onClick={handleLeftImageClick}
-        ></img>
-      </div>
+    <div
+      className={styles.homeBody}
+      style={{ backgroundImage: "url(/tog.jpg)" }}
+    >
+      <div className={styles.homeBodyShadow}>
+        <div className={styles.imageContainer}>
+          <img
+            src={leftImageSource}
+            alt="Image1"
+            className={styles.leftImage}
+            onClick={handleLeftImageClick}
+          ></img>
+        </div>
 
-      <div className={styles.imageContainer}>
-        <img src={"/leftimage2.jpeg"} alt="Image2" className={styles.leftImage2}></img>
-      </div>
-      <div className={styles.homeText}>
-        <h1>
-          Earth Miles
-          <br />
-          for partner
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          <br />
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          <br /> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo,
-          <br />
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.
-          <br /> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
-        </p>
-      </div>
-      <div className={styles.buttonContainer}>
-        <Link href="./createaccount" className={styles.button} role="button">
-          Opret en konto
-        </Link>
+        <div className={styles.imageContainer}>
+          <img
+            src={"/leftimage2.jpeg"}
+            alt="Image2"
+            className={styles.leftImage2}
+          ></img>
+        </div>
+        <div className={styles.homeText}>
+          <h1>
+            Earth Miles
+            <br />
+            for partner
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            <br />
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            <br /> quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo,
+            <br />
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu.
+            <br /> Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt.
+          </p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <Link href="./createaccount" className={styles.button} role="button">
+            Opret en konto
+          </Link>
 
-        <Link href="./login" className={styles.button} role="button">
-          Login
-        </Link>
+          <Link href="./login" className={styles.button} role="button">
+            Login
+          </Link>
+        </div>
       </div>
     </div>
   );
