2e90153007975d4c6029923f5b81ffee7058ca16
Lukas G <luka9938@stud.kea.dk>
2023-12-06 14:21:38 +0100

    fuck shit

--- src/components/NewCampaignData/NewCampaignData.js
+++ src/components/NewCampaignData/NewCampaignData.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import styles from "./NewCampaingData.module.css";
+import styles from "./NewCampaignData.module.css";
 const NewCampaignData = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -105,7 +105,12 @@ const NewCampaignData = () => {
         <label className={styles.label}>
           OR code (we generate one for you)
           <div className={styles.checkbox}>
-            <input className={styles.input} type="text" name="link" value={formData.link} />
+            <input
+              className={styles.input}
+              type="text"
+              name="link"
+              value={formData.link}
+            />
             <input
               type="radio"
               name="redemption"
@@ -118,7 +123,12 @@ const NewCampaignData = () => {
         <label className={styles.label}>
           Direct link to workshop
           <div className={styles.checkbox}>
-            <input className={styles.input} type="text" name="link" value={formData.link} />
+            <input
+              className={styles.input}
+              type="text"
+              name="link"
+              value={formData.link}
+            />
             <input
               type="radio"
               name="redemption"
@@ -131,7 +141,12 @@ const NewCampaignData = () => {
         <label className={styles.label}>
           Promo code (CSV, Text File etc.)
           <div className={styles.checkbox}>
-            <input className={styles.input} type="text" name="link" value={formData.link} />
+            <input
+              className={styles.input}
+              type="text"
+              name="link"
+              value={formData.link}
+            />
             <input
               type="radio"
               name="redemption"
