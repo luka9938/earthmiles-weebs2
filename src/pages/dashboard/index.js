0e62823273c23e19c4312b98afe344f0f6ad4c8c
Lukas G <luka9938@stud.kea.dk>
2023-12-07 12:51:11 +0100

    whoops

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -94,7 +94,11 @@ export default function Dashboard() {
             <section className={styles.profile}>
               <section>
                 <ul className={styles.ul}>
-                  <li className={styles.li}>Personal Info</li>
+                  <li className={styles.li}>
+                    <Link href="./profil" className={styles.p}>
+                      Personal info
+                    </Link>
+                  </li>
                   <li className={styles.li}>How it works</li>
                   <li className={styles.li}>Contact support</li>
                   <li className={styles.li}>
