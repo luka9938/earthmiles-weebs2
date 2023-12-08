ff14fa8ba4bc3c89733bb2027c0848ba71d00042
steffen kroyer <stef5450@stud.kea.dk>
2023-12-08 13:51:20 +0100

    menu added to profil

--- src/pages/profil/index.js
+++ src/pages/profil/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from "./ProfilePage.module.css"; // Import stylingmodulet
+import Menu from "@/components/Menu";
 
 const ProfilePage = () => {
   const [companyName, setCompanyName] = useState("");
@@ -27,69 +28,70 @@ const ProfilePage = () => {
   }, []);
 
   return (
-    <div className={styles.profileContainer}>
-      <div className={styles.logoContainer}>
-        <img src="/wheel brown.png" alt="Logo" />
-      </div>
-
-      <div className={styles.profileBox}>
-        <h2 className={styles.profileTitle}>Firmaprofil</h2>
-        <div className={`${styles.formGroup} ${styles.tallInputField}`}>
-          <label className={styles.labelText}>Firmanavn:</label>
-          <input
-            type="text"
-            value={companyName}
-            onChange={(e) => setCompanyName(e.target.value)}
-            className={styles.inputField}
-          />
+    <div>
+      {" "}
+      <Menu />
+      <div className={styles.profileContainer}>
+        <div className={styles.logoContainer}>
+          <img src="/wheel brown.png" alt="Logo" />
         </div>
 
-        <div className={`${styles.formGroup} ${styles.tallInputField}`}>
-          <label className={styles.labelText}>CVR-nummer:</label>
-          <input
-            type="text"
-            value={cvrNumber}
-            onChange={(e) => setCvrNumber(e.target.value)}
-            className={styles.inputField}
-          />
-        </div>
+        <div className={styles.profileBox}>
+          <h2 className={styles.profileTitle}>Firmaprofil</h2>
+          <div className={`${styles.formGroup} ${styles.tallInputField}`}>
+            <label className={styles.labelText}>Firmanavn:</label>
+            <input
+              type="text"
+              value={companyName}
+              onChange={(e) => setCompanyName(e.target.value)}
+              className={styles.inputField}
+            />
+          </div>
 
-        <div className={`${styles.formGroup} ${styles.tallInputField}`}>
-          <label className={styles.labelText}>Firma URL:</label>
-          <input
-            type="text"
-            value={companyURL}
-            onChange={(e) => setCompanyURL(e.target.value)}
-            className={styles.inputField}
-          />
-        </div>
+          <div className={`${styles.formGroup} ${styles.tallInputField}`}>
+            <label className={styles.labelText}>CVR-nummer:</label>
+            <input
+              type="text"
+              value={cvrNumber}
+              onChange={(e) => setCvrNumber(e.target.value)}
+              className={styles.inputField}
+            />
+          </div>
 
-        <div className={`${styles.formGroup} ${styles.tallInputField}`}>
-          <label className={styles.labelText}>Forretningsadresse:</label>
-          <input
-            type="text"
-            value={businessAddress}
-            onChange={(e) => setBusinessAddress(e.target.value)}
-            className={styles.inputField}
-          />
-        </div>
+          <div className={`${styles.formGroup} ${styles.tallInputField}`}>
+            <label className={styles.labelText}>Firma URL:</label>
+            <input
+              type="text"
+              value={companyURL}
+              onChange={(e) => setCompanyURL(e.target.value)}
+              className={styles.inputField}
+            />
+          </div>
 
-        <div className={`${styles.formGroup} ${styles.tallInputField}`}>
-          <label className={styles.labelText}>Telefonnummer:</label>
-          <input
-            type="text"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            className={styles.inputField}
-          />
-        </div>
+          <div className={`${styles.formGroup} ${styles.tallInputField}`}>
+            <label className={styles.labelText}>Forretningsadresse:</label>
+            <input
+              type="text"
+              value={businessAddress}
+              onChange={(e) => setBusinessAddress(e.target.value)}
+              className={styles.inputField}
+            />
+          </div>
 
-        <button
-          className={styles.saveProfileButton}
-          onClick={handleSaveProfile}
-        >
-          Gem Profil
-        </button>
+          <div className={`${styles.formGroup} ${styles.tallInputField}`}>
+            <label className={styles.labelText}>Telefonnummer:</label>
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className={styles.inputField}
+            />
+          </div>
+
+          <button className={styles.saveProfileButton} onClick={handleSaveProfile}>
+            Gem Profil
+          </button>
+        </div>
       </div>
     </div>
   );
